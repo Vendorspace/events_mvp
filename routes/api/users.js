6ce03ef08d11b7965359130c3486d1bfc2948c27
stkane <stephenw.kane@gmail.com>
2018-05-23 14:25:18 -0400

    nodemailer template added

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,6 +5,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
+const nodemailer = require("nodemailer");
 
 //Load Input Validation
 const validateRegisterInput = require("../../validation/register");
@@ -48,7 +49,53 @@ router.post("/register", (req, res) => {
 					user.password = hash;
 					user
 						.save()
-						.then(encrypt_user => res.json(encrypt_user))
+						.then(encrypt_user => {
+							res.json(encrypt_user);
+
+							nodemailer.createTestAccount((err, account) => {
+								// create reusable transporter object using the default SMTP transport
+								let transporter = nodemailer.createTransport({
+									host: "smtp.ethereal.email",
+									port: 587,
+									secure: false, // true for 465, false for other ports
+									auth: {
+										user: account.user, // generated ethereal user
+										pass: account.pass // generated ethereal password
+									}
+								});
+
+								// setup email data with unicode symbols
+								let mailOptions = {
+									from: '"Fred Foo 👻" <foo@example.com>', // sender address
+									to: `${user.contact_email}`, // list of receivers
+									subject: "Hello ✔", // Subject line
+									text: "Hello world?", // plain text body
+									html: "<b>Hello world?</b>" // html body
+								};
+
+								// send mail with defined transport object
+								transporter.sendMail(
+									mailOptions,
+									(error, info) => {
+										if (error) {
+											return console.log(error);
+										}
+										console.log(
+											"Message sent: %s",
+											info.messageId
+										);
+										// Preview only available when sending through an Ethereal account
+										console.log(
+											"Preview URL: %s",
+											nodemailer.getTestMessageUrl(info)
+										);
+
+										// Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+										// Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+									}
+								);
+							});
+						})
 						.catch(err => console.log(err));
 				});
 			});
