18e1ede8682e58503c1945f29537a95b7bc6926d
stkane <stephenw.kane@gmail.com>
2018-05-25 17:17:10 -0400

    fixed react-ui to client reference

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ app.use("/api/posts", posts);
 
 if(process.env.NODE_ENV === 'production') {
   //set static folder
-  app.use(express.static('react_ui/build'));
+  app.use(express.static('client/build'));
   
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
