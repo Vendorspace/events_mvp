b72fcace33ecd25ece41bacf5b998bd070fe79b7
stkane <stephenw.kane@gmail.com>
2018-07-23 17:41:12 -0600

    search by category route created

--- routes/api/search.js
+++ routes/api/search.js
@@ -17,30 +17,34 @@ router.get("/test", (req, res) => res.json({ message: "search works" }));
 router.get('/usersList/:type/', (req, res) => {
 	User.find({ userType: req.params.type }, (err, users) => {
 		res.send(users);
-	});
+	})
+	.catch(err =>
+		res.status(404)
+		.json({ profile: "not a category"})
+	);
 });
 
 
-router.get("/search/:type/", (req, res) => {
-	const errors = {};
+// router.get("/:type/", (req, res) => {
+// 	const errors = {};
 
-	User.find({ bizType: req.params.type })
-		.populate({
-			model: "User",
-			path: "user",
-			select: ["bizName", "owner"]
-		})
-		.then(profile => {
-			if (!profiles) {
-				errors.noprofile = "There is no profile for this user";
-				res.status(404).json(errors);
-			} 
-			res.json(profiles);
-		})
-		.catch(err =>
-			res
-				.status(404)
-				.json({ profile: "There is no profile for this user" })
-		);
-});
+// 	User.find({ userType: req.params.type })
+// 		.populate({
+// 			model: "User",
+// 			path: "user",
+// 			select: ["bizName", "owner"]
+// 		})
+// 		.then(profile => {
+// 			if (!profiles) {
+// 				errors.noprofile = "There is no kljprofile for this user";
+// 				res.status(404).json(errors);
+// 			} 
+// 			res.json(profiles);
+// 		})
+// 		.catch(err =>
+// 			res
+// 				.status(404)
+// 				.json({ profile: "There is no profile for this user" })
+// 		);
+// });
 module.exports = router;
