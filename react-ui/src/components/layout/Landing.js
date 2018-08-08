138fb89f041c0ad7f2779f884abbb3b46c043ad8
stkane <stephenw.kane@gmail.com>
2018-08-08 16:26:10 -0600

    decomposed searchbar from landing page

--- react-ui/src/components/layout/Landing.js
+++ react-ui/src/components/layout/Landing.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { PropTypes } from "prop-types";
 import { connect } from "react-redux";
 import "../../CSS/landing.css";
+import SearchBar from "../search/SearchBar";
 
 class Landing extends Component {
   componentDidMount() {
@@ -17,35 +18,7 @@ class Landing extends Component {
         <div className="jumbotron">
           <div className="container text-center">
             <h4 className="display-4 ">Your next event starts here.</h4>
-            <div className=" mt-5 pb-5 searchbox">
-              <form className="form-inline justify-content-center">
-                <div className="input-group mt-5 mb-2 mr-sm-2 mb-sm-0">
-                  <input
-                    type="text"
-                    className="form-control"
-                    id="lookingfor"
-                    placeholder="Looking for..."
-                  />
-                  <div className="form-group col-md-4">
-                    <select id="inputState" className="form-control">
-                      <option selected>Business Type</option>
-                      <option>Vendor</option>
-                      <option>Planner</option>
-                      <option>Supplier</option>
-                    </select>
-                  </div>
-                </div>
-              </form>
-              <div className="mt-5 ">
-                <button
-                  id="searchbutton"
-                  type="button"
-                  className=" btn btn-warning p-2 mb-5  btn-primary my-2 my-sm-0"
-                >
-                  Search
-                </button>
-              </div>
-            </div>
+              <SearchBar />
           </div>
         </div>
         <div className="container">
---
+++ react-ui/src/components/search/SearchBar.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import { PropTypes } from "prop-types";
+import { connect } from "react-redux";
+import "../../CSS/landing.css";
+
+export default class SearchBar extends Component {
+  render() {
+    return (
+      <div className=" mt-5 pb-5 searchbox">
+        <form className="form-inline justify-content-center">
+          <div className="input-group mt-5 mb-2 mr-sm-2 mb-sm-0">
+            <input
+              type="text"
+              className="form-control"
+              id="lookingfor"
+              placeholder="Looking for..."
+            />
+            <div className="form-group col-md-4">
+              <select id="inputState" className="form-control">
+                <option selected>Business Type</option>
+                <option>Vendor</option>
+                <option>Planner</option>
+                <option>Supplier</option>
+              </select>
+            </div>
+          </div>
+        </form>
+        <div className="mt-5 ">
+          <button
+            id="searchbutton"
+            type="button"
+            className=" btn btn-warning p-2 mb-5  btn-primary my-2 my-sm-0"
+          >
+            Search
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+
